920d3b78b8a96828b8c413470c38221a47abd193
habibur5313 <habiburmamun313@gmail.com>
2024-09-19 03:26:39 +0600

    second-commit

--- js/home.js
+++ js/home.js
@@ -11,11 +11,48 @@ document.getElementById('add-money-button').addEventListener('click',function(ev
                        
                        document.getElementById('current-balance').innerText = balanceNumber + amountNumber;
                      
-                       document.getElementById('amount-add-money').value = ''
-                       document.getElementById('pin-add-money').value = ''
                     }
                     else{
                                         alert('Wrong INfo! please try again')
                     }
+                    document.getElementById('amount-add-money').value = ''
+                    document.getElementById('pin-add-money').value = ''
 
 })
+
+document.getElementById('cash-out-button').addEventListener('click',function(event){
+                    event.preventDefault();
+                    const currentBalance = document.getElementById('current-balance').innerText;
+                    const inputPin = document.getElementById('pin-cash-out').value;
+                    const inputCashOut = document.getElementById('amount-cash-out').value;
+
+                    if(inputPin === '1234'){
+                                        const balanceNumber = parseFloat(currentBalance);
+                                        const amountNumber = parseFloat(inputCashOut);
+
+                    document.getElementById('current-balance').innerText = balanceNumber - amountNumber;
+                    }
+                    else{
+                                        alert('Wrong INfo! please try again')
+                    }
+                    document.getElementById('amount-cash-out').value = ''
+                       document.getElementById('pin-cash-out').value = ''
+})
+
+
+document.getElementById('Withdraw-btn').addEventListener('click',function(){
+                    const withdraw = document.getElementById('cash-out-form');
+                    withdraw.classList.remove('hidden');
+
+                    const deposit = document.getElementById('add-money-form');
+                    deposit.classList.add('hidden')
+})
+document.getElementById('deposite-btn').addEventListener('click',function(){
+                    const deposit = document.getElementById('add-money-form');
+                    deposit.classList.remove('hidden')
+
+                    const withdraw = document.getElementById('cash-out-form');
+                    withdraw.classList.add('hidden');
+
+})
+
